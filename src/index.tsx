395958d775253a6188c702fd9d5eef0b41a83f55
Bhuvanesh Duvvuri <duvvuribhuvanesh@zohomail.in>
2024-12-10 11:11:27 +0530

    Done

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "./pages/Home.tsx";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+const container = document.querySelector(".root")!;
+const root = createRoot(container);
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </Router>,
+);
